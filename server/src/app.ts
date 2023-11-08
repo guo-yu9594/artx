ad6cc6a89a20d4bea97f31a23ea54c40afe5f892
Guo Yu <guo.yu@epitech.eu>
2023-11-08 01:32:20 +0100

    feat: add a root test route

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,11 +9,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(router);
+
+app.get("/", (_, res) => res.send("Hello World"));
 
 app.listen(port, () => {
   return console.log(
